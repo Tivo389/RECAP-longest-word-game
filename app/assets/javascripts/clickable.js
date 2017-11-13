c34f54824e62057824bff8d4cff1cd0a6a505200
Shun Miyaki <shunmiyaki@hotmail.com>
2017-11-13 23:04:33 +0900

    refactor done

--- app/assets/javascripts/clickable.js
+++ app/assets/javascripts/clickable.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
   // 056 Put the line below to ensure the file was being compiled correctly :)
   // console.log('DOM READY!!');
-  // 057 When the letters on mousedown (not click since it requires user to click-off on theletter) do the following...
-
+  var input = [];
+  // 060 Did some refactoring. 061 is BELOW.
   playWordGame();
 
+  // playWordGame START
   function playWordGame() {
-    var input = [];
+    getUserInput();
+  };
+  // playWordGame END
+
+  // getUserInput START
+  function getUserInput() {
     $('div.grid__container').children().each(function(gridIndex) {
+      // 057 When the letters on mousedown (not click since it requires user to click-off on theletter) do the following...
       $(this).mousedown(function() {
         var selectedAlphabet = this.innerHTML;
         function inputAlphabet(gridIndex, selectedAlphabet) {
@@ -20,9 +27,8 @@ $(document).ready(function() {
             if ($(this).hasClass('selected') === false) {
               input.push(new inputAlphabet(gridIndex, selectedAlphabet));
               $(this).addClass('selected');
-              console.log(input);
+              // 061 Make a new function that will duplicate (this? i think) into the word div area. CONTINUE HERE
               return input;
-              break;
             } else {
               input.forEach(function(object, arrayIndex) {
                 // Index of letter in the grid.
@@ -34,18 +40,17 @@ $(document).ready(function() {
                 if (object[gridIndex] === selectedAlphabet) delete input[arrayIndex];
               });
               $(this).removeClass('selected');
-              console.log(input);
               return input;
-              break;
             }
           case 2: // Middle
             break;
           case 3: // Right
             break;
         }
-        // 059 After 058 was complete I needed to show the same letters in the & pass it to the input. 060 is ABOVE.
       });
     });
   };
+  // 059 After 058 was complete I needed to show the same letters to the user & pass it to the input. 060 is ABOVE.
+  // getUserInput END
 
 });
